fafa68e3989c99472ef51266f5df5178a2a5adec
Dragunkeysystem <prahlad3492@gmail.com>
2026-05-03 01:42:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -51,7 +51,7 @@ local Button = MainTab:CreateButton({
 local SpeedKey = Enum.KeyCode.LeftControl
 
 local SpeedKeyMultiplier = 3
-local FlightSpeed = 150
+local FlightSpeed = 200
 local FlightAcceleration = 4
 local TurnSpeed = 16
 
